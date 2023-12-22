c1463f0473326767b16d8e7146e5f6dc592936e3
Jay Kapadia <jayskapadia@gmail.com>
2023-12-21 22:50:31 -0700

    first edit

--- hello.js
+++ hello.js
@@ -1,3 +1,3 @@
-const hello = () => "Hello World from the Office Hours in the terminal!";
+const hello = () => "Hello World from the MIT CTO Program from Arizona - Jay Kapadia! ";
 console.log(hello());
 exports.hello = hello;
